523f5fe65cbfee39cfcfa9b5d06d4a774e3f3580
glozxi <glozxi@gmail.com>
2023-05-03 10:37:12 +0800

    Add color change on hover

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const innerDiv = document.createElement("div");
         innerDiv.classList.add("small-box");
+        innerDiv.addEventListener("mouseover", () => colorIn(innerDiv));
         div.append(innerDiv);
     }
     container.append(div);
+}
+
+function colorIn(box) {
+    box.style.backgroundColor = "black";
 }
